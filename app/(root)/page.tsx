f894113118d2ed3de25bdee75957d52327e77bcd
apoorvbansal95 <apoorv95.ab@gmail.com>
2025-10-16 13:10:38 -0400

    header for app

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div className='flex items-center justify-center h-screen'>
+            <Button>
+                click me
+            </Button>
+        </div>
+    )
+}
+export default Page
+
